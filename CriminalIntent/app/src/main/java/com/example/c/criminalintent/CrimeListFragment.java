05612563ecce87c93bd3d118b8b7a4c5e2d300d4
kimuijin <webpro2@naver.com>
2016-07-30 18:24:57 +0900

    add viewpager

--- CriminalIntent/app/src/main/java/com/example/c/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/com/example/c/criminalintent/CrimeListFragment.java
@@ -1,13 +1,18 @@
 package com.example.c.criminalintent;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -28,13 +33,52 @@ public class CrimeListFragment extends Fragment {
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
     }
 
+    class CrimeAdapter extends ArrayAdapter<Crime>{
+        public CrimeAdapter(){
+            super(getActivity(),0,mCrimes);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null){
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_crime,null);
+            }
+            Crime c = mCrimes.get(position);
+            TextView titleTextView = (TextView)convertView.findViewById(R.id.crime_list_item_titleTextView);
+            TextView dateTextView = (TextView)convertView.findViewById(R.id.crime_list_item_dateTextView);
+            CheckBox solvedCheckBox = (CheckBox)convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+            titleTextView.setText(c.getTitle());
+            dateTextView.setText(c.getDate().toString());
+            solvedCheckBox.setChecked(c.isSolved());
+            return  convertView;
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_crime_list, container, false);
         crimeListView = (ListView)v.findViewById(R.id.crimeListView);
+//        ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(),android.R.layout.simple_list_item_1,mCrimes);
+        CrimeAdapter adapter = new CrimeAdapter();
+        crimeListView.setAdapter(adapter);
+        crimeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Crime crime = mCrimes.get(position);
+                Intent intent = new Intent(getActivity(),CrimePagerActivity.class);
+                intent.putExtra(CrimeFragment.EXTRA_ID,crime.getId());
+                startActivity(intent);
+            }
+        });
         return v;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        CrimeAdapter adapter = (CrimeAdapter)crimeListView.getAdapter();
+        adapter.notifyDataSetChanged();
+    }
 }
